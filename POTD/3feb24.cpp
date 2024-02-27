5c56886269da45e6e3080017f21edb7baf6de589
sahitya <sahityaaryansingh@gmail.com>
2024-02-27 21:55:55 +0530

    POTD/3feb

---
+++ POTD/3feb24.cpp
@@ -0,0 +1,29 @@
+
+
+// # date: 3 feb 2024
+// # link: https://www.geeksforgeeks.org/problems/decimal-equivalent-of-binary-linked-list/1
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long int s = (long int) 10e9;
+            int mod = 10e8 + 7;
+            int m = 1000000007;    
+    //  cout << s + 7;
+    cout <<'\n' <<mod << '\n' << m;
+    cout << endl;
+    return 0;
+
+
+}
+
+
+
+// 89
+// 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 0 1 1 1 0 1 0 1 0 1 1 1 1 1 1 0 1 1 0 0 0 0 0 0 1 0 1 1 0 1 1 1 0 0 0 1 0 1 1 0 1 1 0 0 0 1 1 1 1 1 0 1 1 0 1 1 0 0 0 1 0 0 1 0 1 1 0 1 1 1 1 0 1 0
+
+// 32
+// 0111111111111111111111010101010111111111111
