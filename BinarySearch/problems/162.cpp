0918662bd58e49b190f0d3c8f0b83737ebca279c
sahitya <sahityaaryansingh@gmail.com>
2024-03-04 00:15:24 +0530

    162.cpp

---
+++ BinarySearch/problems/162.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int findPeakElement(vector<int>& nums) {
+
+        int n = nums.size() , lo  =0, hi = n-1;
+
+        if (n==1) return 0;
+
+        while(lo <= hi){
+
+            int mid = lo + (hi-lo)/2;
+
+            if(mid==n-1) return mid; // reason becasue mid +1 will give overflow and my code will handle the lower cases 
+
+            if(nums[mid] < nums[mid+1]) {
+
+                lo = mid + 1;
+
+            } else {
+                hi = mid - 1;
+            }
+                
+        }
+        
+        return lo;
+    }
+
+int main()
+{
+ 
+    
+    cout << endl;
+    return 0;
+}
