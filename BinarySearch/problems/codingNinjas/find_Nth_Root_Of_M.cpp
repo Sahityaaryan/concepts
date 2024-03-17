3400b6b9a65e53e02edc0f10dc7bfed626f21d24
sahitya <sahityaaryansingh@gmail.com>
2024-03-17 05:45:05 +0530

    corrected the nth root of the M

--- BinarySearch/problems/codingNinjas/find_Nth_Root_Of_M.cpp
+++ BinarySearch/problems/codingNinjas/find_Nth_Root_Of_M.cpp
@@ -7,40 +7,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long mulToNtimes(int number, int n){
-  long long multiplier = number;
-  long long num = number;
-  while(n--){
-    num *= multiplier;
-  }
+//* Here the best part is that we are checking for the overflow condition at the moment we are calculating the multiply to n times
 
-  return num/multiplier;
-}
 
-int NthRoot(int n, int m) {
+long long mul(int n, int k, int m)
+{
 
-  // checking the for the ntimes multiply of the 
-
-  int lo = 1, hi = m/n;
-
-  while(lo <= hi){
-
-    int mid = lo + (hi-lo)/2;
-
-    long long assumedMultiple = mulToNtimes(mid,n); 
-
-    if(assumedMultiple==(long long)m) return mid;
-    else if(assumedMultiple > (long long)m){
-        hi = mid-1;
-    } else {
-      lo = mid + 1;
+    long long ans = 1;
+    for (int i = 0; i < k; i++)
+    {
+        ans *= (long long)n;
+        if(ans > m) {
+            ans = -1;
+            break;
+        }
     }
-  }
 
-  return -1;
+    return ans;
 }
 
+int NthRoot(int n, int m)
+{
+    if (n == 1)
+        return m;
 
+    int hi = (m >= 4) ? m / 2 : m;
+
+    int lo = 1, mid;
+    long long compute;
+
+
+    while (lo <= hi)
+    {
+        mid = lo + (hi - lo) / 2;
+
+        compute = mul(mid, n, m);
+
+        if(compute == -1){
+             hi = mid-1;
+             continue;
+        }
+
+
+        if (compute < (long long) m)
+        {
+            lo = mid + 1;
+        }
+        else if (compute > (long long) m)
+        {
+            hi = mid - 1;
+        }
+        else
+        {
+            return mid;
+        }
+
+    }
+
+    return -1;
+}
 // 217013
 
 int main()
