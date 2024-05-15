b8a47c5dddd3e4f79f21b00df07b5d8b7b848a21
sahitya <sahityaaryansingh@gmail.com>
2024-05-15 23:35:51 +0530

    added the 1021.cpp

---
+++ string/1021.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+    string str = " sahityaaryan";
+    // cout << str.substr(3,4); // strt + length 
+    char s = ' ';
+    cout << (int) s;
+    cout << endl;
+    return 0;
+}
