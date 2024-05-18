8dc26c71a6e10df199dcad0a9359add7f9ce3436
sahitya <sahityaaryansingh@gmail.com>
2024-05-18 09:16:36 +0530

    finally solved the binary_search in 2d matrix

--- BinarySearch/bs_in_2d/median_in_2d.cpp
+++ BinarySearch/bs_in_2d/median_in_2d.cpp
@@ -1,122 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+// striver says that: T.C = R * log(C)
+
+// me =  T.C = log(R*C)*(R * log(C)); // because we actually want to find that number who is in middle of that array which is formed when all the elements are included into it in sorted order.
 
 // this will take a long time to fix things on the place
 
-int median(vector<vector<int>> &matrix, int m, int n) {
-    int ps = (m*n)/2, midRow,midCol;
-    int lo = 1,hi = ps,mid,mid1,mid2;
-    int rowPosit1,rowPosit2,rowPosit3,rowPosit4;
-    int colPosit1,colPosit2,colPosit3,colPosit4;
-    int leftUpper,leftLower,rightUpper,rightLower;
-    int mini1,mini2;
+int calculateTheLessNumber(vector<vector<int>> v , int mid){
 
-    midRow = ps/m,midCol = ps%n-1;
+    int i  = 0,j = 0,count = 0;
 
-    if(ps%m==0){
-        midRow--;
-    } 
+    while(i < v.size()){
 
-    if(ps%n==0){
-        midCol = n-1;
-    }
-
-    while(lo <= hi){
-        mid = lo + (hi-lo)/2;
-        mid1 = mid;
-        mid2 = ps-mid;
-
-        // calculating row and column indices of the leftn upper
-
-        int rankOfLeftLower = ps + mid2 -1;
-
-        rowPosit1 = mid1;rowPosit3 = rowPosit1;
-
-        if(mid1%m==0){// when we have chosen the last member of a row
-            rowPosit1--;
-        }
-
-        colPosit1 = mid1%n-1;colPosit3 = colPosit1+1;
-
-        if(mid1%n==0){ // when we have chosen the last member of a row
-            colPosit1 = n-1;
-            colPosit3 = 0;
-        }
-
-        int i = 0;
-        leftUpper = matrix[rowPosit1][colPosit1];
-
-        while(i < rowPosit1){
-            leftUpper = max(leftUpper,matrix[i][n-1]);
-            i++;
-        }
-
-        // leftUpper
-
-        rowPosit2 = rankOfLeftLower/m;rowPosit4 = rowPosit2;
-
-        if(mid2%m==0){// when we have chosen the last member of a row
-            rowPosit2--;
-        }
-
-        colPosit2 = rankOfLeftLower%n-1;colPosit4 = colPosit2+1;
-
-        if(mid2%n==0){ // when we have chosen the last member of a row
-            colPosit2 = n-1;
-            colPosit4 = 0;
-        }
-
-
-        leftLower = matrix[rowPosit2][colPosit4];
+        j = 0;
         
-        while(i < rowPosit2){ // check here !!!!!!!!!!!!
-            leftLower = max(leftLower, matrix[i][n-1]);
-            i++;
+        int lo = 0;
+        int hi = v[0].size() - 1;
+
+        while(lo <= hi){
+            int md = lo + (hi-lo)/2;
+
+            if(mid < v[i][md]){
+                hi = md-1;
+            } else {
+                lo = md + 1;
+            } 
+            j++;
         }
 
-        // stiching
-
-        rightUpper = matrix[rowPosit2][colPosit2];
-
-            
-
-        i = rowPosit3+1;
-
-        while(i <= midRow){
-            rightUpper = min(rightUpper,matrix[i][0]);
-            i++;
-        }
-
-        rightLower = matrix[rowPosit4][colPosit4];
-
-        i = rowPosit4 == m-1 ? m-1:rowPosit4+1;
-
-        while(i < m){
-            rightLower = min(rightLower,matrix[i][0]);
-            i++;
-        }
-
-
-        if(rightUpper < leftLower && rightLower < leftUpper){
-            return min(rightLower,rightUpper);
-        } 
-
-        if(rightUpper > leftLower ){
-            hi = mid-1;
-        } else {
-            lo = mid+1;
-        }
-
+        count += lo;
+        i++;
     }
 
-    return -1;        
+    return count;
+}
 
+int median(vector<vector<int>> &matrix, int R, int C)
+{
+    int mid, lo = INT_MAX, hi = INT_MIN, i = 0, j = 0, count = (R * C + 1) / 2;
+
+    while (i < R)
+    {
+        lo = min(matrix[i][0], lo);
+        hi = max(matrix[i][C - 1], hi);
+        i++;
+    }
+
+    i = 0;
+
+    while (lo < hi){
+        mid = (lo + hi) / 2;
+
+        if (count <= calculateTheLessNumber(matrix,mid)){
+            hi = mid;
+        }
+        else {
+            lo = mid + 1;
+        }
+    }
+
+    return lo;
 }
 
 int main()
 {
- 
-    
+
+    vector<vector<int>> v = {{2, 3, 5}, {2, 3, 4}, {1, 7, 9}};
+
+    cout << "answer: " << median(v, v.size(), v[0].size());
+
     cout << endl;
     return 0;
 }
