a13bed3540865a99604e0ec5bfe6a22b1ef1bc7b
sahitya <sahityaaryansingh@gmail.com>
2024-05-25 01:10:34 +0530

    added the normalisation concept of string inorder to check whether the strings are isomorphic or not

--- string/205.cpp
+++ string/205.cpp
@@ -1,25 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
- 
-    string s = "Hello, world";
-    string t = "elppk hkrpb";
-    unordered_map<char,char> mp;
 
+// new method of checking whether it is isomorphic string or not
+
+void normalise(string &s){
+    unordered_map<char,char>mp;
+    char tipAlphabet = 'a';
+
+    for(char ch:s){
+        if(!mp[ch]){
+            mp[ch] = tipAlphabet;
+            tipAlphabet++;
+        }
+    }
     int i = 0;
 
     while(i < s.length()){
-        cout <<(int) mp[s[i]] << endl;
-        char g = ' ';
-        if(mp[s[i]]==g){
-            cout << "got it\n";
-        }
+        s[i] = mp[s[i]];
         i++;
     }
+}
+
+bool isoMorphic
+
+   bool isIsomorphic(string s, string t) {
+        
+        // first check: size must be same
+
+        if(s.size() != t.size()) return false;
+
+// second check : we will actually check the mapping of the function
+
+        unordered_map<char,char>mp1,mp2;
+
+        int i = 0;
+
+        
+
+        while(i < s.size()){
+            
+            if((int)mp1[s[i]] == 0){
+                if((int)mp2[t[i]] == 0){
+                    mp1[s[i]] = t[i];
+                    mp2[t[i]] = s[i];
+                } else {
+                    return false;
+                }
+            }
+
+            if(mp1[s[i]] != t[i]){
+                return false;
+            }
+            i++;
+        }
+
+        return true;
+       
+    }
+
+int main()
+{
+    string s2,s1;
+
+    s1 = "add";
+    s2 = "egg";
+    normalise(s2);
+    normalise(s1);
+
+    cout << "after normalisation " << s1<< endl;
+    cout << "before normalisation " <<  s2 << endl;
 
 
-    
 
     cout << endl;
     return 0;
