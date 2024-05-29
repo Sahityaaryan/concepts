1039b0f0ffd24e92ac6d26f95b4729604c09b6a6
sahitya <sahityaaryansingh@gmail.com>
2024-05-29 15:26:06 +0530

    finally the done the 5

--- string/5.cpp
+++ string/5.cpp
@@ -1,61 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+bool isPalin(string s, int l, int r){
+    bool flag = true;
+    int i = 0;
+    while (i <= ((r - l + 1) / 2))
+    {
+        if (s[l + i] != s[r - i])
+        {
+            flag = false;
+            break;
+        }
+        i++;
+    }
+    return flag;
+}
 
-      bool isPalin(string s, int l, int r) {
-        bool flag = true;
-        int i = 0;
-        while (i <= ((r - l + 1) / 2)) {
-            if (s[l + i] != s[r - i]) {
-                flag = false;
-                break;
+string longestPalindrome(string s)
+{
+    if(!(s.length()-1)){
+        return s;
+    }
+
+    if(isPalin(s,0,s.length()-1)){
+        return s;
+    }
+
+    int leader = 0, width = 0;
+    int lagger = 0;
+
+    vector<int> ans(1001);
+    int n = s.length();
+
+    while (lagger < n){
+
+        if(leader == n){
+            lagger++;
+            leader = lagger;
+        }
+
+        if (s[leader] == s[lagger])
+        {
+            if (isPalin(s, lagger, leader) && leader != lagger)
+            {
+                // cout << "lagger: "<< lagger  << " leader: " << leader << endl;
+
+                if(width < leader-lagger+1){
+                width =  leader - lagger + 1;
+                ans[width] = lagger;
+                }
             }
-            i++;
-        }
-        return flag;
-    }
 
-    string longestPalindrome(string s) {
-
-        int leader = 0, width = 0;
-        int lagger = 0;
-
-        // vector<int> ans (1001);
-        int requiredLagger=s.length();
-        int n = s.length();
-
-
-         while (leader < n) {
-           if(s[leader] == s[lagger] && leader != lagger)
-           {
-            cout << "leader " << leader << " lagger: " << lagger << endl;
-               if(isPalin(s,lagger,leader))
-               {
-                   if(leader-lagger+1 > width){
-                    requiredLagger = lagger;
-                    width = leader - lagger + 1;
-                   }
-                   
-               } else {
+            if (leader != lagger)
                 lagger++;
-               }
-            
-           }
-           leader++;
         }
-
-
-        return s.substr(requiredLagger, width);
+        leader++;
     }
+ 
+
+    if(!ans[width] && !width){
+        return s.substr(0,1);
+    }
+    
+    // cout << "width: " << width  << "lagger: " << ans[width]<< endl;
+    return s.substr(ans[width], width);
+}
 
 int main()
 {
     string s = "cbbd";
-    cout << longestPalindrome(s) << endl;
-    // cout << endl;
-    // s = "babad";
     // cout << longestPalindrome(s) << endl;
     // cout << endl;
-    
+    s = "babad";
+    s = "aacabdkacaa";
+    cout << longestPalindrome(s) << endl;
+    // cout << endl;
+
     cout << endl;
     return 0;
 }
