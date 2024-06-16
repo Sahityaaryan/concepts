f969dc13b7c93c8c455f3775257f139fc29541f2
sahitya <sahityaaryansingh@gmail.com>
2024-06-16 08:13:03 +0530

    bs

--- BinarySearch/bs_in_2d/median_in_2d.cpp
+++ BinarySearch/bs_in_2d/median_in_2d.cpp
@@ -8,18 +8,15 @@ using namespace std;
 // me =  T.C = log(R*C)*(R * log(C)); // because we actually want to find that number who is in middle of that array which is formed when all the elements are included into it in sorted order.
 
 // this will take a long time to fix things on the place
-// Main Idea; the medai has fixed number of numbers before it.
+// Main Idea; the median has fixed number of numbers before it.
 
 // ***** Questions: 
-//! 1. first hwo it is making sure that the number is existing in the vector means it is caluclating 
+//! 1. first how it is making sure that the number is existing in the vector means it is caluclating 
 // just a mid without making sure wheter the never is existing or not ?
 
-// * Ans: Got it actually if you see the mechanism cleary so you will find that it is trying to get the lowest number whose count is greater than the  number
-//* of count (elements before the median without including it) and also searching for the highest number whose number of elements before and including it
-//* is just greater than count. --> so by this basically we are finding the lowest possible answer whose number of elements before and including it is equal 
-//* to the count which is nothing but the median itself and that's how we are making sure that the assumed median is existing. 
+// * Ans: Got it actually if you see the mechanism cleary so you will find that it is trying to get the lowest number whose count is greater than the  number of count (elements before the median without including it) and also searching for the highest number whose number of elements before and including it is just greater than count. --> so by this basically we are finding the lowest possible answer whose number of elements before and including it is equal to the count which is nothing but the median itself and that's how we are making sure that the assumed median is existing. 
 
-//! 2. why we have to give lo as to increment the count? while calculating the (totla number  of numbers less than the given number in the entire array)\
+//! 2. why we have to give lo as to increment the count? while calculating the (total number  of numbers less than the given number in the entire array)\
 // * Ans: Just check the upperBound in the BinarySearch/problems dir.
 
 
