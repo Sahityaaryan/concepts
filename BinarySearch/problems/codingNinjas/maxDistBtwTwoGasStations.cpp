ab2ed9f09046e79a236c83f86f4585d469e76d2a
sahitya <sahityaaryansingh@gmail.com>
2024-07-15 14:00:56 +0530

    imporved the code for maxDistanceBtwGasStations

--- BinarySearch/problems/codingNinjas/maxDistBtwTwoGasStations.cpp
+++ BinarySearch/problems/codingNinjas/maxDistBtwTwoGasStations.cpp
@@ -8,52 +8,44 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-    bool isAmple(vector<int> arr,long double mid, int k){
-
-
-        int i = 0, placed = 0;
-        int dist;
-
-        while(i < arr.size()-1){
-
-            
-            int numberOfPlacedStations = (arr[i+1]-arr[i])/mid;
-
-            if(dist  == numberOfPlacedStations*mid) numberOfPlacedStations--;
-
-            placed += numberOfPlacedStations;
-            i++;
-
+    int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size(); // size of the array
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
         }
-        return placed<=k;
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
+
+  
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        vector<int> arr = stations;
+      int n = arr.size(); // size of the array
+    long double low = 0;
+    long double high = 0;
+
+    //Find the maximum distance:
+    for (int i = 0; i < n - 1; i++) {
+        high = max(high, (long double)(arr[i + 1] - arr[i]));
     }
 
-
-    double minimiseMaxDistance(vector<int> &arr, int k){
-        
-        long double  hi,lo;
-        hi=lo=0.0;
-
-        int i =0, dist;
-
-        while(i < arr.size()){
-
-            if( (long double) (arr[i+1]-arr[i]) > hi){
-                hi = (long double) arr[i+1]-arr[i];
-            }
-            i++;
+    //Apply Binary search:
+    long double diff = 1e-6 ;
+    while (high - low > diff) {
+        long double mid = (low + high) / (2.0);
+        int cnt = numberOfGasStationsRequired(mid, arr);
+        if (cnt > k) {
+            low = mid;
         }
-
-        while(hi-lo > 1e-6){
-            long double mid  = (lo + hi)/2.0;
-
-            if(isAmple(arr, mid, k)){
-                hi = mid;
-            } else {
-                lo = mid;
-            }
+        else {
+            high = mid;
         }
-        return hi;
+    }
+    return high;
     }
 
 
