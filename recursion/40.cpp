6baa9c139aa427256b34fad9d5ee2db1f2f7149d
sahitya <sahityaaryansingh@gmail.com>
2024-07-20 12:04:04 +0530

    40.cpp

---
+++ recursion/40.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+
+    vector<int> nums = {1,2,3};
+
+    int i = 0;
+
+    vector<vector<int>> bs;
+
+    bs.push_back({1,2,3});
+
+    if(bs[0]==nums){
+        cout << "it is working" << endl;
+    }
+    
+    cout << endl;
+    return 0;
+}
