9d18ef8ded9da735cd258cfbb345e1b81a9342c2
sahityaaryan <sahityaaryansingh@gmail.com>
2024-09-19 06:59:26 +0530

    19 sept

--- Stack_+_Queue/146.cpp
+++ Stack_+_Queue/146.cpp
@@ -86,13 +86,6 @@ public:
 };
 
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
-
 
 int main(){
 
