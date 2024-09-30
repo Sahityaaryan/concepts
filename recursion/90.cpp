af8e10b33ac0d8abb7338d4b765edfb9c52824ba
sahityaaryan <sahityaaryansingh@gmail.com>
2024-10-01 02:30:51 +0530

    1 oct

--- recursion/90.cpp
+++ recursion/90.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+void print(vector<int>& v){
+
+    cout << "{ ";
+
+    for(auto it:v){
+        cout << " " << it;
+    }
+    
+    cout << " }";
+}
+
 void sol(int ind, vector<int>& tempArr, vector<vector<int>>& ans, vector<int>& arr){
 
+
+    cout << ind << ' ';
+
+    print(tempArr);
+
     ans.emplace_back(tempArr);
+
+    cout << '\n';
 
     if(ind == arr.size()) return;
 
@@ -14,6 +33,7 @@ void sol(int ind, vector<int>& tempArr, vector<vector<int>>& ans, vector<int>& a
         tempArr.emplace_back(arr[i]);
         sol(i+1,tempArr,ans,arr);
         tempArr.pop_back();
+        // if(i == arr.size()-1) cout << "end\n"; 
     }
 }
 
@@ -30,6 +50,10 @@ void sol(int ind, vector<int>& tempArr, vector<vector<int>>& ans, vector<int>& a
 
 int main()
 {
+
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> ans = subsetsWithDup(nums);
+
     cout << endl;
     return 0;
 }
