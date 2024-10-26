b628c3c71200166fce0e24f31d95182aaa1277fc
sahityaaryan <sahityaaryansingh@gmail.com>
2024-10-27 01:30:25 +0530

    explained concept for twoNumberswithOddOccurrence

--- BitManipulation/gfg/twoNumbersWithOddOccurences.cpp
+++ BitManipulation/gfg/twoNumbersWithOddOccurences.cpp
@@ -13,6 +13,10 @@ using namespace std;
         long long int res2 = 0;
         
         int first_rightmost_different_bit = xor_res & ~(xor_res-1);
+
+        // here first_rightmost_different_bit is actually an even number like "1000" where the positon of set bit represent the first rightmost different bit
+        // as till the rightmost '1' all the '0' are representing similar bits of both the numbers so by this info different of the bit at the 4th place from
+        // right we head to find the xor of number which has a set bit at the 4th place and the unset bit at the 4th place 
         
         for(long long int i = 0;i < N; i++){
             if(Arr[i] & first_rightmost_different_bit){
