e0e2706349f17d3e5c28d9c7d5c6777625c6f425
sahityaaryan <sahityaaryansingh@gmail.com>
2024-12-05 01:26:08 +0530

    5 december

--- BinaryHeap/practice/median_in_datastream.cpp
+++ BinaryHeap/practice/median_in_datastream.cpp
@@ -80,3 +80,50 @@ cout << "~" << "\n";
 	return 0;
 }
 // } Driver Code Ends
+
+class MedianFinder {
+public:
+        priority_queue<int> s;
+        priority_queue<int, vector<int>, greater<int>> g;
+    MedianFinder() {
+
+        
+    }
+    
+    void addNum(int num) {
+        
+        if(s.size() == 0){
+            s.push(num);
+            return;
+        }
+
+        if(s.size() != g.size()){
+
+            if(num < s.top()){ // abhi num top se chota and size unequal hai toh mujhe size equal karni hai mujhe s.top() ko maxlist mein dalna hoga and then use add karenge
+                g.push(s.top());
+                s.pop();
+                s.push(num);
+            } else { // aur if num greater hai then toh use maxlist main hi daal do
+                g.push(num);
+            }
+        } else {
+
+            if(num < s.top()){ // dekho agar num chota hai min list ke max se toh direct usme daal do
+                s.push(num);
+            } else { // agar wo bada hai toh chances ho sakte hai usse koi chota maxlist me present ho toh isliye use maxList mein dal do aur wo minheap hai toh minimum top pe aa jayega and use small list mein dal do
+                g.push(num);
+                s.push(g.top());
+                g.pop();
+            }
+        }
+    }
+    
+    double findMedian() {
+        
+        if((s.size() + g.size()) & 1){
+            return s.top();
+        }
+
+        return (double) (s.top() + g.top()) / 2.0;
+    }
+};
