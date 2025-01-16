d463ef7292bad8cc27863336f53fbf7cbe9586a4
sahityaaryan <sahityaaryansingh@gmail.com>
2025-01-16 19:26:22 +0530

    prim's algorithm using priority queue

--- graphs/mst.cpp
+++ graphs/mst.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
  
+/*
+
+
+    1. using the loop approach
 
   int spanningTree(int V, vector<vector<int>> adj[]) {
        
@@ -46,6 +50,68 @@ using namespace std;
        return mst_val;
        
     }
+
+    // T.C : O(V*(V + V)) = O(V^2 + 2V)
+    // S.C : O(2V)
+
+
+
+    2. using priority_queue for little optimization at time
+
+        int spanningTree(int V, vector<vector<int>> adj[]) {
+        
+        // prim's algorithm
+       
+       int mst_val = 0;
+       int min_key = -1;
+       int number_of_nodes_connected = 0;
+       bool found_min = true;
+       
+       priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> key;
+       
+       vector<int> mst(V, false);
+   
+        
+       key.push({0, 0});
+        
+       
+       while(!key.empty()){
+           found_min = false;
+           
+           while(mst[key.top().second]){
+               key.pop();
+           }
+           
+           if(key.empty()) break;
+           
+           min_key = key.top().second;
+           mst[min_key] = true;
+           mst_val += key.top().first;
+           number_of_nodes_connected++;
+           
+           key.pop();
+           
+        //   finding adjacent nodes
+        
+            for(auto nodes:adj[min_key]){
+                int v = nodes[0];
+                int weight = nodes[1];
+                
+               if(!mst[v]) key.push({weight, v});
+            }
+            
+            if(number_of_nodes_connected == V) break;
+       }
+       
+       return mst_val;
+       
+       
+    //   T.C : O(V*(V + logV)) = O(V^2 + V*logV)
+    // S.C : O(2V)
+
+
+
+    */
 
 int main(){
     
