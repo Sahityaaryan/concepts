1078af6e5bb7eab54024a300b683f424bfe179b2
sahityaaryan <sahityaaryansingh@gmail.com>
2025-07-23 17:34:38 +0530

    Explaination for the bottom-up approach for mcm

--- dp/mcm/bottom-up.cpp
+++ dp/mcm/bottom-up.cpp
@@ -25,19 +25,21 @@ using namespace std;
  *
  * # Rules
  * 1. remember the base case (i==j) dp[i][j] = 0;
- * 2.
+ * 2. and everything even the logic remains the same as it was for the (top-down approach)
+ *
+ * but what does (dp[i][j]) signifies
+ * dp[i][j] => what is the least number of calculations you have to do while multiplying every matriix from ith position to n-1th position (indexing is according to the given array)
  *
  */
 
- class Solution {
-   public:
+
 
      int matrixMultiplication(vector<int> &arr) {
          const int n = sz(arr);
 
          ll dp[n][n];
 
-         for(int i = 1; i < n; ++i){
+         for(int i = 0; i < n; ++i){
              dp[i][i] = 0ll;
          }
 
